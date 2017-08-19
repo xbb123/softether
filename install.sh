c1030d36930e5f032d23371c0dd1cdb358a31cab
xbb123 <xbb123@users.noreply.github.com>
2017-08-19 18:33:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ tar -zxvf softether-vpnserver-v4.18-9570-rtm-2015.07.26-linux-x86-32bit.tar.gz
 function set64 {
 wget http://www.softether-download.com/files/softether/v4.22-9634-beta-2016.11.27-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.22-9634-beta-2016.11.27-linux-x64-64bit.tar.gz
 #wget http://www.softether-download.com/files/softether/v4.17-9562-beta-2015.05.30-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.17-9562-beta-2015.05.30-linux-x64-64bit.tar.gz
-tar softether-vpnserver-v4.22-9634-beta-2016.11.27-linux-x64-64bit.tar.gz
+tar -zxvf softether-vpnserver-v4.22-9634-beta-2016.11.27-linux-x64-64bit.tar.gz
 }
 
 # function doselect {
